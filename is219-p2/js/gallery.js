67e81ecded82ecee4af6da4648ea97441aa4f9ce
MaitreyPatel <maitreyptl@gmail.com>
2018-03-31 16:23:57 -0400

    Create javascript object GalleryImage

--- is219-p2/js/gallery.js
+++ is219-p2/js/gallery.js
@@ -45,7 +45,6 @@ var mCurrentIndex = 0;
 
 // XMLHttpRequest variable
 var mRequest = new XMLHttpRequest();
-
 // Array holding GalleryImage objects (see below).
 var mImages = [];
 
@@ -54,7 +53,8 @@ var mJson;
 
 // URL for the JSON to load by default
 // Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
-var mUrl = 'insert_url_here_to_image_json';
+var mUrl = 'images.json';
+
 
 
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
@@ -79,7 +79,11 @@ window.addEventListener('load', function() {
 
 }, false);
 
-function GalleryImage() {
+function GalleryImage(location, description, date, img) {
+	this.location = location;
+	this.description = description;
+	this.date = date;
+	this.img = img;
 	//implement me as an object to hold the following data about an image:
 	//1. location where photo was taken
 	//2. description of photo

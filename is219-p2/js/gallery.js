0e928f733ab0e295891237cf7ef6a0fd95f7bf23
MaitreyPatel <maitreyptl@gmail.com>
2018-04-01 18:17:55 -0400

    Testing Bugs in functions

--- is219-p2/js/gallery.js
+++ is219-p2/js/gallery.js
@@ -33,106 +33,76 @@ function animate() {
 /************* DO NOT TOUCH CODE ABOVE THIS LINE ***************/
 // Counter for the mImages array
 var mCurrentIndex = null;
+
 function swapPhoto() {
-	//Add code here to access the #slideShow element.
-	//Access the img element and replace its source
-	//with a new image from your images array which is loaded 
-	//from the JSON string
-	if (mCurrentIndex = null) {
-		mCurrentIndex = 0;
-	}
-	else if (mCurrentIndex == mImages.length - 1){
-		mCurrentIndex = 0;
-	}
-	else {
-		mCurrentIndex++;
-	}
-	const location = "Location: ",description = "Description: ",date = "Date: ";
+	if(mCurrentIndex == null){ mCurrentIndex = 0; }
+
+	else if (mCurrentIndex ==  mImages.length - 1) { mCurrentIndex = 0; }
+
+	else { mCurrentIndex++; }
+
+	const location = "Location: ", description = "Description: ", date = "Date: ";
 	var photoLocation = mImages[mCurrentIndex].imgLocation;
-	var descripPhoto = mImages[mCurrentIndex].description;
-	var datePhoto = mImages[mCurrentIndex].date;
-	
+	var photoDesc = mImages[mCurrentIndex].description;
+	var photoDate = mImages[mCurrentIndex].date;
 	console.log('swap photo');
-	
 	$("#photo").attr("src", mImages[mCurrentIndex].imgPath);
 	console.log(mImages[mCurrentIndex].imgPath);
 	$(".location").text(location + photoLocation);
-	$(".description").text(desciption + descripPhoto);
-	$(".date").text(date + datePhoto);
-	
+	$(".description").text(description + photoDesc);
+	$(".date").text(date + photoDate);
+
 	console.log(photoLocation);
 	console.log(mCurrentIndex + " mCurrentIndex on swapPhoto");
 }
-function prevPhoto() {
-	if (mCurrentIndex = null) {
-		mCurrentIndex = mImages.length -1;
-	}
-	else if (mCurrentIndex == 0){
-		mCurrentIndex = mImages.length -1;
-	}
-	else {
-		mCurrentIndex--;
-	}
-	const location = "Location: ",description = "Description: ",date = "Date: ";
+
+function PrevPhoto(){
+
+	if(mCurrentIndex == null){ mCurrentIndex = mImages.length - 1; }
+
+	else if(mCurrentIndex == 0){ mCurrentIndex = mImages.length - 1; }
+
+	else { mCurrentIndex--; }
+
+	const location = "Location: ", description = "Description: ", date = "Date: ";
 	var photoLocation = mImages[mCurrentIndex].imgLocation;
-	var descripPhoto = mImages[mCurrentIndex].description;
-	var datePhoto = mImages[mCurrentIndex].date;
-	
-	
-	
+	var photoDesc = mImages[mCurrentIndex].description;
+	var photoDate = mImages[mCurrentIndex].date;
 	$("#photo").attr("src", mImages[mCurrentIndex].imgPath);
 	console.log(mImages[mCurrentIndex].imgPath);
 	$(".location").text(location + photoLocation);
-	$(".description").text(desciption + descripPhoto);
-	$(".date").text(date + datePhoto);
-	
+	$(".description").text(description + photoDesc);
+	$(".date").text(date + photoDate);
+
 	console.log(photoLocation);
 	console.log(mCurrentIndex + " mCurrentIndex on swapPhoto");
 }
 
-
-/* URL for the JSON to load by default */
-// Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
 
 // XMLHttpRequest variable
-var mUrl = 'images.json';
+var mURL = "images.json";
 var mRequest = new XMLHttpRequest();
 
-var m2URL = 'extra.json';
-var m2Request = new XMLHttpRequest();
+var mURL2 = "extra.json";
+var mRequest2 = new XMLHttpRequest();
+
 // Array holding GalleryImage objects (see below).
 var mImages = [];
 
 // Holds the retrived JSON information
 mRequest.onreadystatechange = function() {
-	if (mRequest.readyState == 4 && mRequest.status == 200) {
-		try {
-			var mJson = JSON.parse(mRequest.responseText);
-			for (var i = 0; i < mJson.images.length; i++) {
-				mImages.push(new GalleryImage(mJson.images[i].imgPath, mJson.images[i].imgLocation, mJson.images[i].description, mJson.images[i].date));
-			}
-			console.log(mJson.images);
-			console.log(mJson.images[0].imgPath);
-		}
-		catch (err) {
-			console.log(err.message)
-		}
-	}
-};
-
-m2Request.onreadystatechange = function() {
   // Do something interesting if file is opened successfully
-  if (m2Request.readyState == 4 && m2Request.status == 200) {
+  if (mRequest.readyState == 4 && mRequest.status == 200) {
     try {
       // Let’s try and see if we can parse JSON
-      var m2Json = JSON.parse(m2Request.responseText);
+      var mJson = JSON.parse(mRequest.responseText);
 
-      for (var i = 0; i < m2Json.images.length; i++){
-      	mImages.push(new GalleryImage(m2Json.images[i].imgPath, m2Json.images[i].imgLocation, m2Json.images[i].description, m2Json.images[i].date));
+      for (var i = 0; i < mJson.images.length; i++){
+      	mImages.push(new GalleryImage(mJson.images[i].imgPath, mJson.images[i].imgLocation, mJson.images[i].description, mJson.images[i].date));
       }
       // Let’s print out the JSON; It will likely show as “obj”
-      console.log(m2Json.images);
-      console.log(m2Json.images[0].imgPath);
+      console.log(mJson.images);
+      console.log(mJson.images[0].imgPath);
 
      
     } catch (err) {
@@ -140,13 +110,33 @@ m2Request.onreadystatechange = function() {
     }
   }
 };
-				
+
+mRequest2.onreadystatechange = function() {
+  // Do something interesting if file is opened successfully
+  if (mRequest2.readyState == 4 && mRequest2.status == 200) {
+    try {
+      // Let’s try and see if we can parse JSON
+      var mJson2 = JSON.parse(mRequest2.responseText);
+
+      for (var i = 0; i < mJson2.images.length; i++){
+      	mImages.push(new GalleryImage(mJson2.images[i].imgPath, mJson2.images[i].imgLocation, mJson2.images[i].description, mJson2.images[i].date));
+      }
+      // Let’s print out the JSON; It will likely show as “obj”
+      console.log(mJson2.images);
+      console.log(mJson2.images[0].imgPath);
+
+     
+    } catch (err) {
+      console.log(err.message)
+    }
+  }
+};
+
 mRequest.open("GET", mURL, true);
 mRequest.send();
 
-m2Request.open("GET", m2URL, true);
-m2Request.send();
-
+mRequest2.open("GET", mURL2, true);
+mRequest2.send();
 
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
 //@param A GalleryImage object. Use this method for an event handler for loading a gallery Image object (optional).
@@ -158,13 +148,13 @@ function makeGalleryImageOnloadCallback(galleryImage) {
 }
 
 $(document).ready( function() {
-	
 	// This initially hides the photos' metadata information
 	$('.details').eq(0).hide();
-	
-	$('img.moreIndicator').click( function() {
-		$('.deatils').eq(0).toggle();
+
+	$('img.moreIndicator').click( function(){
+		$('.details').eq(0).toggle();
 	});
+
 	$(".moreIndicator").click( function(){
 		if( $(".moreIndicator").hasClass("rot90")){
 			$(".moreIndicator").attr("class", "moreIndicator rot270");
@@ -173,11 +163,13 @@ $(document).ready( function() {
 			$(".moreIndicator").attr("class", "moreIndicator rot90");
 		};
 	});
+
 	$('#prevPhoto').click( function(){
 		mLastFrameTime = 0;
 		PrevPhoto();
 		animate();
 	});
+
 	$('#nextPhoto').click(function(){
 		mLastFrameTime = 0;
 		swapPhoto();
@@ -192,14 +184,9 @@ window.addEventListener('load', function() {
 
 }, false);
 
-function GalleryImage(imgLocation, description, date, imgPath) {
+function GalleryImage(imgPath, imgLocation, description, date){
+	this.imgPath = imgPath;
 	this.imgLocation = imgLocation;
 	this.description = description;
 	this.date = date;
-	this.imgPath = imgPath;
-	//implement me as an object to hold the following data about an image:
-	//1. location where photo was taken
-	//2. description of photo
-	//3. the date when the photo was taken
-	//4. either a String (src URL) or an an HTMLImageObject (bitmap of the photo. https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement)
-}
+};
